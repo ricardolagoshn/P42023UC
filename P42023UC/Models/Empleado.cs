76ba2bd84939f4b80f779a50129883fca3239daf
ricardo.lagos <ricardo.lagoshn@gmail.com>
2023-02-13 22:26:00 -0600

    Add project files.

---
+++ P42023UC/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P42023UC.NewFolder
+{
+    public class Empleado 
+    {
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set;}
+        public string foto { get; set; }
+    }
+}
---
+++ P42023UC/PagePrincipal.xaml.cs
@@ -0,0 +1,39 @@
+﻿using P42023UC.NewFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace P42023UC
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PagePrincipal : ContentPage
+	{
+		public PagePrincipal ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnprocesar_Clicked(object sender, EventArgs e)
+        {
+			var emple = new Empleado
+			{
+				id = 1,
+				nombres = txtnombre.Text,
+				apellidos = txtapellido.Text,
+				foto = null
+			};
+
+			// esto es para llamar una seguna pagina y pasarle parametro a una seguna pantalla
+			var secondpage = new PageResultado();
+			secondpage.BindingContext = emple;  // paso de parametros a la pantalla
+			Navigation.PushAsync(secondpage);
+
+			// Estoy sin audio un momento no me habia dado cuenta
+        }
+    }
+}
