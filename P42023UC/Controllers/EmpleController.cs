0bad6795cd55c1035466f0127f9ecdc776c12897
ricardo.lagos <ricardo.lagoshn@gmail.com>
2023-02-25 15:22:36 -0600

    Cambios realizados al 25/02/2023

---
+++ P42023UC/Controllers/EmpleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using P42023UC.Models;
+using System.Threading.Tasks;
+
+namespace P42023UC.Controllers
+{
+    public class EmpleController
+    {
+        readonly SQLiteAsyncConnection _connection;
+        // Constructor de clase
+        public EmpleController(string DBPath) 
+        {
+            _connection = new SQLiteAsyncConnection(DBPath);
+            _connection.CreateTableAsync<Models.Empleado>();
+        }
+
+        // CRUD - Create - Read - Update - Delete
+
+        // Create / Update
+        public Task<int> SaveEmple(Models.Empleado empleado) 
+        {
+            if (empleado.id != 0)
+                return _connection.UpdateAsync(empleado);
+            else
+                return _connection.InsertAsync(empleado);
+        }
+
+        //Read one un empleado
+        public Task<Models.Empleado> GetEmpleado(int pid) 
+        {
+            return _connection.Table<Models.Empleado>()
+                .Where(i => i.id == pid)
+                .FirstOrDefaultAsync();
+        }
+
+        // Read para toda la lista de empleados
+        public Task<List<Models.Empleado>> GetListEmple() 
+        {
+            return _connection.Table<Models.Empleado>().ToListAsync();
+        }
+
+        //Delete 
+        public Task<int> DeleteEmple(Models.Empleado empleado) 
+        {
+            return _connection.DeleteAsync(empleado);
+        }
+
+    }
+}
--- P42023UC/Models/Empleado.cs
+++ P42023UC/Models/Empleado.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
+using SQLite;
 
-namespace P42023UC.NewFolder
+namespace P42023UC.Models
 {
     public class Empleado 
     {
+        [PrimaryKey, AutoIncrement]
         public int id { get; set; }
+
+        [MaxLength(100)]
         public string nombres { get; set; }
+
+        [MaxLength(100)]
         public string apellidos { get; set;}
         public string foto { get; set; }
     }
